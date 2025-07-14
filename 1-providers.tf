680d95b8cbc356e392de3b604a6d8024a9ee18a7
tanya-domi <sheytanyadominic@gmail.com>
2025-07-14 11:54:13 +0200

    updated workflows

--- 1-providers.tf
+++ 1-providers.tf
@@ -14,7 +14,7 @@ terraform {
 
 # Configure the backend to use s3 for state storage
     backend "s3" {
-    bucket = "berlin-41"
+    bucket = "berlin-42"
     key    = "tool/terraform.tfstate"
     region = "us-east-1"
   }
